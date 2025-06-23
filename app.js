3d2b1bd19f9f012ee97c5076c22ca24bc0b50107
anaushad246 <anaushad246@gmail.com>
2025-06-23 11:06:28 +0530

    fix: started as new 23-6-25 10:39 am

--- app.js
+++ app.js
@@ -21,10 +21,17 @@ const uploadsDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir);
 }
+import dotenv from 'dotenv';
+dotenv.config();
+console.log(process.env.CORS_ORIGIN);
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
+// app.use(express.static("public"));
+const allowedOrigin = process.env.CORS_ORIGIN;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
-  // credentials: true
+  origin: allowedOrigin,
+  credentials: true // if you use cookies or authorization headers
 }));
 
 app.use(express.json({limit:"16kb"}));
