e78c8307dd3dcb8374384a23d7a100d2ec6c7e0a
anaushad246 <anaushad246@gmail.com>
2025-06-23 10:51:40 +0530

    fix: started as new 23-6-25 10:39 am

--- app.js
+++ app.js
@@ -22,25 +22,10 @@ if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir);
 }
 
-const allowedOrigins = [
-  'https://fact-check-frontend-lovat.vercel.app',
-  'https://fact-check-frontend-git-main-naushad-ahmads-projects.vercel.app',
-  'https://fact-check-frontend-blwoyoyp9-naushad-ahmads-projects.vercel.app',
-  'http://localhost:5173'  // Optional: useful for local development
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    console.log("CORS Origin:", origin); // Helpful for debugging
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS: " + origin));
-    }
-  },
+  origin: 'http://localhost:5173',
   credentials: true
 }));
-
 
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
